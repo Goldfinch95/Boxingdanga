0852299a94b944f172c1ca3f42e44c4d102a72c2
Goldfinch <facu_vila@hotmail.com>
2024-02-10 21:51:32 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'login';
+  faGoogle = faGoogle;
+  faFacebookF = faFacebookF;
+
+  clickedCreateAccount = false;
+  clickedForgotPassword = false;
+  formHidden: boolean = false;
+
+  callCreateAccount(){
+    if(this.clickedCreateAccount == false){
+      this.formHidden = true;
+      this.clickedCreateAccount = true;
+    }
+  }
+
+  callforgotPassword(){
+    if(this.clickedForgotPassword == false){
+      this.formHidden = true;
+      this.clickedForgotPassword = true;
+    }
+  }
 }
